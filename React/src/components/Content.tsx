a38fff729d2991fd177d2ed4a8c38124c4c87ad2
MilesMavis <emil.suorsa@gmail.com>
2024-06-28 08:42:18 +0300

    9.15

--- React/src/components/Content.tsx
+++ React/src/components/Content.tsx
@@ -1,16 +1,60 @@
 /* eslint-disable react/react-in-jsx-scope */
-interface CoursePart {
-  name: string;
-  exerciseCount: number;
+import CoursePart from "../types";
+
+function Part(props: CoursePart) {
+  let additionalContent: JSX.Element;
+
+  switch (props.kind) {
+    case "basic":
+      additionalContent = <i>{props.description}</i>;
+      break;
+    case "group":
+      additionalContent = (
+        <p>project exercises {props.groupProjectCount.toString()}</p>
+      );
+      break;
+    case "background":
+      additionalContent = (
+        <div>
+          <i>{props.description}</i> <p>submit to {props.backgroundMaterial}</p>
+        </div>
+      );
+      break;
+    case "special":
+      additionalContent = (
+        <div>
+          <i>{props.description}</i>{" "}
+          <p>
+            required skills{" "}
+            {props.requirements.toString()}
+          </p>
+        </div>
+      );
+      break;
+    default:
+      additionalContent = <div></div>;
+      break;
+  }
+
+  return (
+    <div>
+      <h3>
+        {props.name} {props.exerciseCount}
+      </h3>
+      {additionalContent}
+    </div>
+  );
 }
 
-export default function Content({ content }: { content: CoursePart[] }) {
+interface ContentProps {
+  content: CoursePart[];
+}
+
+export default function Content({ content }: ContentProps) {
   return (
     <div>
       {content.map((coursePart, i) => (
-        <p key={i}>
-          {coursePart.name} {coursePart.exerciseCount}
-        </p>
+        <Part key={i} {...coursePart} />
       ))}
     </div>
   );
